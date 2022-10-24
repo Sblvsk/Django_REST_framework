80db8b995ecbd17cbecb04b98bb2d25e27cdbfc2
Sergey <smr.kolesnikov@gmail.com>
2022-10-24 22:13:37 +0500

    begin api

--- TODO/frontend/src/App.js
+++ TODO/frontend/src/App.js
@@ -1,19 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import UserList from './components/User.js';
+import axios from 'axios';
+
 
 class App extends React.Component {
  constructor(props) {
     super(props);
     this.state = {
-    "users": []
+      "users": []
     }
   }
-  render(){
-  return (
-    <div>
-        Main
-    </div>
+
+  componentDidMount() {
+    axios.get("http://127.0.0.1:8013/api/users/").then(response => {
+        this.setState(
+        {
+          "users":response.data
+        }
+    )
+
+    })
+
+//    const users = [
+//      {
+//        'first_name':'Фёдор',
+//        'last_name':'Достоевский',
+//        'age':1822
+//      },
+//      {
+//        'first_name':'Александр',
+//        'last_name':'Грин',
+//        'age': 1880,
+//      }
+//
+//    ]
+
+  }
+
+  render() {
+    return (
+      <div>
+         <UserList users={this.state.users} />
+      </div>
     )
   }
 }
