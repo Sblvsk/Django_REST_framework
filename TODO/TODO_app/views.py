a1a2917a25612c1eb2c89f712a09ada2e57789db
Sergey <smr.kolesnikov@gmail.com>
2022-11-16 21:38:07 +0500

    pagination ready

--- TODO/TODO_app/views.py
+++ TODO/TODO_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import status
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.response import Response
 
 from rest_framework.viewsets import ModelViewSet
@@ -9,18 +10,25 @@ from .models import User, Project, Todo
 from .serializers import ProjectSerializer, TodoSerializer
 
 
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
+
+class TodoLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 20
+
 
 class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
     filterset_class = ProjectFilterSet
+    pagination_class = ProjectLimitOffsetPagination
 
 
 class TodoModelViewSet(ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
     filterset_fields = ['project']
-    #filterset_class = TodoFilterSet
+    pagination_class = TodoLimitOffsetPagination
 
 
     def destroy(self, request, *args, **kwargs):
