e2d730f9207ab907c4234add89d0cdcaf3517793
Sergey <smr.kolesnikov@gmail.com>
2022-11-16 15:55:43 +0500

    settings ready

--- TODO/TODO/settings.py
+++ TODO/TODO/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'users',
     'corsheaders',
     'TODO_app',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer', ],
+
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100
+}
