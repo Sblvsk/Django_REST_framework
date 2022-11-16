bf594081f2d86dc9738cee6c5b0423439aaf8660
Sergey <smr.kolesnikov@gmail.com>
2022-11-16 20:49:05 +0500

    Todo.ViewSet.Destroy ready

--- TODO/TODO_app/views.py
+++ TODO/TODO_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
 
 from rest_framework.viewsets import ModelViewSet
 
@@ -15,3 +17,20 @@ class ProjectModelViewSet(ModelViewSet):
 class TodoModelViewSet(ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.activate = False
+        instance.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+    # def destroy(self, request, *args, **kwargs):
+    #     instance = self.get_object()
+    #     self.perform_destroy(instance)
+    #     return Response(status=status.HTTP_204_NO_CONTENT)
+    #
+    # def perform_destroy(self, instance):
+    #     instance.delete()
+
