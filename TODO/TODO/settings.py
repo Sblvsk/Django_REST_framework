84645a280d75d2c5e84a930a7e391de51bf28d02
Sergey <smr.kolesnikov@gmail.com>
2022-11-29 18:15:35 +0500

    swagger

--- TODO/TODO/settings.py
+++ TODO/TODO/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django_filters',
     'rest_framework.authtoken',
     'projectAPIapp',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- TODO/TODO/urls.py
+++ TODO/TODO/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework.permissions import AllowAny
 
 from rest_framework.routers import DefaultRouter
 
@@ -23,18 +26,30 @@ from users.views import UserCustomViewSet
 from TODO_app.views import ProjectModelViewSet, TodoModelViewSet
 from rest_framework.authtoken import views
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Todo',
+        default_version='v1',
+        description='Test API',
+        license=openapi.License(name='MIT License')
+    ),
+    public=True,
+    permission_classes=(AllowAny,),
+)
+
 router = DefaultRouter()
 router.register('users', UserCustomViewSet)
 router.register('projects', ProjectModelViewSet)
 router.register('todo', TodoModelViewSet)
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth', include('rest_framework.urls')),
     path('api/', include(router.urls)),
     path('api-token-auth/', views.obtain_auth_token),
-    path('api/<str:version>/projects/', ProjectListAPIView.as_view()),
 
+    path('api/<str:version>/projects/', ProjectListAPIView.as_view()),
+    # path('swagger<str:format>/', schema_view.without_ui()),
+    path('swagger/', schema_view.with_ui('swagger')),
 
 ]
